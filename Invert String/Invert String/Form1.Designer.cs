3a6e52f5c18d176a0fab5f43f5544b53a2a0b040
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-01-28 13:00:51 +0330

    Add files via upload

---
+++ Invert String/Invert String/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Invert_String
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnInvert
+            // 
+            this.btnInvert.Location = new System.Drawing.Point(146, 130);
+            this.btnInvert.Name = "btnInvert";
+            this.btnInvert.Size = new System.Drawing.Size(103, 23);
+            this.btnInvert.TabIndex = 0;
+            this.btnInvert.Text = "Show Invert String ";
+            this.btnInvert.UseVisualStyleBackColor = true;
+            this.btnInvert.Click += new System.EventHandler(this.btnInvert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter String:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(146, 95);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInvert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ Invert String/Invert String/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Invert_String
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInvert_Click(object sender, EventArgs e)
+        {
+            string main_string = txtInput.Text;
+            string invert_string = "";
+            foreach (char ch in main_string)
+                invert_string = ch.ToString() + invert_string;
+            MessageBox.Show("Invert String=" + "\"" + invert_string + "\"");
+        }
+    }
+}
